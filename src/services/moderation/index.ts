4da5e0a62628ee8940e5a6d3f64bf23bc67458e4
Tudy Gbaguidi <tudy.gbaguidi@epitech.eu>
2026-04-09 10:26:24 +0000

    chore: remove image moderation feature (not shipping)

--- src/services/moderation/index.ts
+++
@@ -1,4 +0,0 @@
-export { tfliteService } from "./tflite.service";
-export type { GateResult } from "./tflite.service";
-export { gateChatImageBeforeSend } from "./gate-chat-image";
-export type { GateChatImageResult } from "./gate-chat-image";
